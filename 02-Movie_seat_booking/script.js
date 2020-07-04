bd4b67b0b336ad4eedbc723e13a21e4c0a9d422c
raminhafezi <raminhafezi@gmail.com>
2020-07-05 00:23:08 +1000

    working with local storage and update UI in every page refresh

--- 02-Movie_seat_booking/script.js
+++ 02-Movie_seat_booking/script.js
@@ -4,15 +4,61 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
-const ticketPrice = +movieSelect.value;
+movieSelect.seatIndex = 3;
+let ticketPrice = +movieSelect.value;
+
+// get data from localStorange and update UI
+const populateUI = () => {
+  //Retrive and Update Selected Seat
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  selectedSeats !== null && selectedSeats.length > 0;
+  seats.forEach((seat, index) => {
+    selectedSeats.indexOf(index) > -1 ? seat.classList.add("selected") : null;
+  });
+
+  // Retrive and Update Selected Movie
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  selectedMovieIndex !== null
+    ? (movieSelect.selectedIndex = selectedMovieIndex)
+    : null;
+
+  // update count and total price
+  updateSelectedCount();
+};
+
+// saved selected movie data and price
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+};
+
+const updateSelectedCount = () => {
+  const selectedSeat = document.querySelectorAll(".row .seat.selected");
+  const seatIndex = [...selectedSeat].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatIndex));
+
+  // console.log(seatIndex);
+  const selectedSeatsCount = +selectedSeat.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = +selectedSeatsCount * ticketPrice;
+};
 
 const seatSelectHandler = (event) => {
   event.target.classList.contains("seat") &&
   !event.target.classList.contains("occupied")
     ? event.target.classList.toggle("selected")
     : "";
+  updateSelectedCount();
+};
+
+const updateTickerPrice = (event) => {
+  ticketPrice = +event.target.value;
+  updateSelectedCount();
+  setMovieData(event.target.selectedIndex, ticketPrice);
+  // setMovieData(event.target);
 };
 
 container.addEventListener("click", seatSelectHandler);
+movieSelect.addEventListener("change", updateTickerPrice);
 
-console.log(ticketPrice);
+populateUI();
