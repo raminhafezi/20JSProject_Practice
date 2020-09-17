41e76f2674a0e4702704a8949fa32e640d062ec9
raminhafezi <raminhafezi@gmail.com>
2020-09-17 14:10:59 +1000

    all fucntions are working, doible money, sort, add and remove users, total wealth

--- 05-DOM Array Methods /WC/raha-random-users/src/components/my-component/raha-random-users.tsx
+++ 05-DOM Array Methods /WC/raha-random-users/src/components/my-component/raha-random-users.tsx
@@ -38,6 +38,44 @@ export class MyComponent {
     // return result;
   }
 
+  //   double the wealth of each person in the list inplace.
+  doubleMoney = () => {
+    this.data.forEach(person => (person.money += person.money));
+    this.updateDOM();
+  };
+
+  //   filter out all non millioners from the list and replace the data
+  showMillionersOnly = () => {
+    this.data = this.data.filter(person => person.money > 1000000);
+    this.updateDOM();
+  };
+
+  //   Sort in ascending order, sort in place
+  sortData = () => {
+    this.data.sort((p1, p2) => {
+      return p1.money > p2.money ? -100 : p1.money == p2.money ? 0 : 100;
+    });
+    this.updateDOM();
+  };
+
+  //   calculate total welath of the people on data and display as in an h3 tag, at the end of the list
+  calculateTotalWelth = () => {
+    const total = this.data.reduce((total, person) => {
+      return (total += person.money);
+    }, 0);
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth : <strong>$${this.numberWithCommas(total)}</storng></h3>`;
+    this.main.appendChild(wealthEl);
+  };
+
+  removeUser = event => {
+    if (event.target.nodeName === 'STRONG') {
+      this.data = this.data.filter(person => {
+        return person.name !== event.target.innerHTML;
+      });
+    }
+    this.updateDOM();
+  };
   // Show numers in thiusand seprator commas and return as string
   numberWithCommas = x => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
@@ -50,12 +88,20 @@ export class MyComponent {
           <button id="add-user" onClick={this.getRandomUser}>
             Add User 🤗
           </button>
-          <button id="double">Double Money 💰</button>
-          <button id="show-millioners">Show Only Millioners💎</button>
-          <button id="sort">Sort by Richest 😲</button>
-          <button id="calculate-wealth">Calculate entire Wealth 👑</button>
+          <button id="double" onClick={this.doubleMoney}>
+            Double Money 💰
+          </button>
+          <button id="show-millioners" onClick={this.showMillionersOnly}>
+            Show Only Millioners💎
+          </button>
+          <button id="sort" onClick={this.sortData}>
+            Sort by Richest 😲
+          </button>
+          <button id="calculate-wealth" onClick={this.calculateTotalWelth}>
+            Calculate entire Wealth 👑
+          </button>
         </aside>
-        <main id="main" ref={el => (this.main = el)}>
+        <main id="main" ref={el => (this.main = el)} onClick={this.removeUser}>
           <h2>
             Person<strong>Wealth</strong>
           </h2>
